5338512e832531af44e40c870032d2a4428e1d89
강예린 <jjykhs0505@naver.com>
2024-06-02 16:29:27 +0900

    goseumi images slide js 구현

--- src/js/goseumi_image_slide.js
+++ src/js/goseumi_image_slide.js
@@ -1,21 +1,21 @@
-let nowImage = 1;
+let goseumiCurrentImage = 1;
+const goseumiTotalImages = 3;
+const goseumiImageWidth = 210;
 
-document.querySelector('.prev').addEventListener('click', function () {
-if (nowImage === 2) {
-    document.querySelector('.project-image-container').style.transform = 'translateX(0)';
-    nowImage -= 1;
-} else if (nowImage === 3) {
-    document.querySelector('.project-image-container').style.transform = 'translateX(-210px)';
-    nowImage -= 1;
-}
+document.querySelector('#goseumi-prev-btn').addEventListener('click', function () {
+    if (goseumiCurrentImage > 1) {
+        goseumiCurrentImage--;
+        document.querySelector('.project-image-container-goseumi').style.transform = `translateX(-${goseumiImageWidth * (goseumiCurrentImage - 1)}px)`;
+    }
+
+    document.getElementById("goseumi-current-number").textContent = goseumiCurrentImage;
 });
 
-document.querySelector('.next').addEventListener('click', function () {
-if (nowImage === 1) {
-    document.querySelector('.project-image-container').style.transform = 'translateX(-210px)';
-    nowImage += 1;
-} else if (nowImage === 2) {
-    document.querySelector('.project-image-container').style.transform = 'translateX(-420px)';
-    nowImage += 1;
-}
+document.querySelector('#goseumi-next-btn').addEventListener('click', function () {
+    if (goseumiCurrentImage < goseumiTotalImages) {
+        goseumiCurrentImage++;
+        document.querySelector('.project-image-container-goseumi').style.transform = `translateX(-${goseumiImageWidth * (goseumiCurrentImage - 1)}px)`;
+    }
+
+    document.getElementById("goseumi-current-number").textContent = goseumiCurrentImage;
 });
