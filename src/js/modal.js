86eadc2175648ca6406d1ba99220f43e6e286909
강예린 <jjykhs0505@naver.com>
2024-06-03 21:59:34 +0900

    modal js 구현

---
+++ src/js/modal.js
@@ -0,0 +1,9 @@
+function showModal(modalId) {
+    let modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    let modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
